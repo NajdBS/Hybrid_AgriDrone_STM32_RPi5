232f9fa417fa985a5298497417ce093a1ccb71f7
NajdBS <najdbensaad2106@gmai.com>
2025-11-18 13:10:32 +0100

    CHANGE Project Folder

---
+++ Testdrone/Core/Inc/MPU6050.h
@@ -0,0 +1,56 @@
+/*
+ * MPU6050.h
+ *
+ *  Created on: Nov 15, 2025
+ *      Author: Najd Ben Saad
+ */
+/*
+ * mpu6050.h
+ *
+ *  Created on: Nov 13, 2019
+ *      Author: Bulanov Konstantin
+ */
+#ifndef INC_MPU6050_H_
+#define INC_MPU6050_H_
+
+#include <stdint.h>
+#include "stm32f4xx_hal.h"    // ou #include "stm32f4xx_hal_i2c.h"
+
+extern I2C_HandleTypeDef hi2c1;
+
+// MPU6050 structure
+typedef struct {
+
+    int16_t Accel_X_RAW;
+    int16_t Accel_Y_RAW;
+    int16_t Accel_Z_RAW;
+    double Ax;
+    double Ay;
+    double Az;
+
+    int16_t Gyro_X_RAW;
+    int16_t Gyro_Y_RAW;
+    int16_t Gyro_Z_RAW;
+    double Gx;
+    double Gy;
+    double Gz;
+
+    float Temperature;
+
+    double KalmanAngleX;
+    double KalmanAngleY;
+} MPU6050_t;
+
+
+uint8_t MPU6050_Init(I2C_HandleTypeDef *I2Cx);
+
+void MPU6050_Read_Accel(I2C_HandleTypeDef *I2Cx, MPU6050_t *DataStruct);
+
+void MPU6050_Read_Gyro(I2C_HandleTypeDef *I2Cx, MPU6050_t *DataStruct);
+
+void MPU6050_Read_Temp(I2C_HandleTypeDef *I2Cx, MPU6050_t *DataStruct);
+
+void MPU6050_Read_All(I2C_HandleTypeDef *I2Cx, MPU6050_t *DataStruct);
+
+
+#endif /* INC_MPU6050_H_ */
