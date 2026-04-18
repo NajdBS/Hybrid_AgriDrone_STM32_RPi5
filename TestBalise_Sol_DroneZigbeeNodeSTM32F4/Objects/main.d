f0d762928eed0369db81d286e95777def1fb4b61
NajdBS <najdbensaad2106@gmai.com>
2026-04-18 18:21:06 +0200

    cleanup: remove old project files

--- TestBalise_Sol_DroneZigbeeNodeSTM32F4/Objects/main.d
+++
@@ -1,6 +0,0 @@
-./objects/main.o: main.c \
-  C:\Users\Najd\ Ben\ Saad\AppData\Local\Arm\Packs\Keil\STM32F4xx_DFP\2.17.1\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f4xx.h \
-  C:\Users\Najd\ Ben\ Saad\AppData\Local\Arm\Packs\Keil\STM32F4xx_DFP\2.17.1\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f407xx.h \
-  C:\Users\Najd\ Ben\ Saad\AppData\Local\Arm\Packs\ARM\CMSIS\6.3.0\CMSIS\Core\Include\core_cm4.h \
-  C:\Users\Najd\ Ben\ Saad\AppData\Local\Arm\Packs\Keil\STM32F4xx_DFP\2.17.1\Drivers\CMSIS\Device\ST\STM32F4xx\Include\system_stm32f4xx.h \
-  USART.h UTIL.h ADC.h I2C.h
--- TestDrone_STM32L432KCU/Debug/Core/Src/KalmanRollPitch.d
+++
@@ -1,3 +0,0 @@
-Core/Src/KalmanRollPitch.o: ../Core/Src/KalmanRollPitch.c \
- ../Core/Inc/KalmanRollPitch.h
-../Core/Inc/KalmanRollPitch.h:
